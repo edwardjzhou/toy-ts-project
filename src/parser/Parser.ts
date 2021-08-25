6b9e59c417c68d375b008b7146c8ab4c2ac4d298
edward <edward@utexas.edu>
2021-08-25 01:15:16 -0700

    redo pasre

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
+import readline from 'readline'
 import { measure } from './decorators/measure';
 import { StringCleaning } from './modules/StringCleaning';
 import { AppFileTypes } from './namespaces/AppFileTypes';
-import type { Model, Record, ModelRecord } from './../models/schema';
 
 type Csv = typeof AppFileTypes.csv;
 type Json = typeof AppFileTypes.json;
@@ -24,10 +24,28 @@ interface ParseResult {
     records: string[];
 }
 export class CsvTableParser {  
-  @measure
-  public run(filePath: FilePath<Csv>): Promise<ParseResult>{    
 
-    
+
+  @measure
+  public async run(fp: FilePath<Csv>) {    
+    const rl = readline.createInterface({
+      input: fs.createReadStream(fp),
+      crlfDelay: Infinity
+    });
+
+    const headersArray = [];
+    const records = []
+    let count = 0
+    for await (const line of rl) {
+      if (count++ === 0) headersArray.push(line);
+      else records.push(line)
+    }
+
+    return { headers: headersArray, records: records };
+
+  }
+  // public run(fp: FilePath<Csv>): Promise<ParseResult>{    
+
     // return new Promise(resolve => { 
     //     fs.readFile(filePath, 'utf8' , (err, rawData) => {
     //       if (err) throw err;
@@ -37,32 +55,32 @@ export class CsvTableParser {
     //       resolve(result);
     //     })
     // })
-  }
+  // }
 
-  public read(rawData: string): [header[], string[]] {
-    const headersArray = this.readHeaders(rawData);
-    const rowStringsArray = this.readRows(rawData);
-    return [headersArray, rowStringsArray];
-  }
-  private readRows(rawData: string): string[] {
-    const splitByLine = rawData.split('\n'); 
-    const rowStringsArray = splitByLine.slice(1);
-    return rowStringsArray;
-  }
-  private readHeaders(rawData: string): header[] {
-    const splitByLine = rawData.split('\n'); 
-    const headersString = splitByLine[0];
-    const headersArray = headersString.split(',');
-    return headersArray;
-  }
+  // public read(rawData: string): [header[], string[]] {
+  //   const headersArray = this.readHeaders(rawData);
+  //   const rowStringsArray = this.readRows(rawData);
+  //   return [headersArray, rowStringsArray];
+  // }
+  // private readRows(rawData: string): string[] {
+  //   const splitByLine = rawData.split('\n'); 
+  //   const rowStringsArray = splitByLine.slice(1);
+  //   return rowStringsArray;
+  // }
+  // private readHeaders(rawData: string): header[] {
+  //   const splitByLine = rawData.split('\n'); 
+  //   const headersString = splitByLine[0];
+  //   const headersArray = headersString.split(',');
+  //   return headersArray;
+  // }
 
-  public clean(rowsStringsArray: string[]): void {
-    this.cleanRowStrings(rowsStringsArray);
-  }
-  private cleanRowStrings(rowsStringsArray: string[]): void {  // mutates array
-      StringCleaning.removeEmptyStringsFromStringArray(rowsStringsArray); // mutates
-      StringCleaning.removeSpacesAfterCommasFromStringArray(rowsStringsArray); // mutates
-  }
+  // public clean(rowsStringsArray: string[]): void {
+  //   this.cleanRowStrings(rowsStringsArray);
+  // }
+  // private cleanRowStrings(rowsStringsArray: string[]): void {  // mutates array
+  //     StringCleaning.removeEmptyStringsFromStringArray(rowsStringsArray); // mutates
+  //     StringCleaning.removeSpacesAfterCommasFromStringArray(rowsStringsArray); // mutates
+  // }
 }
 
 export default { CsvTableParser }
